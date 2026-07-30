a8839c2401fa9deeeca3bf494625d2cf5f929223
Marc Mengel <mengel@fnal.gov>
2026-07-30 15:19:07 -0500

    hmm...

--- subspack/subspack.py
+++ subspack/subspack.py
@@ -159,7 +159,7 @@ def quick_clone_repos(prefix, args):
         for repo_name in roots:
             tty.debug(f"repo {repo_name}")
             tty.debug(f"roots[repo_name] is {repr(roots[repo_name])}")
-            branch = roots[repo_name]["branch"]
+            branch = roots[repo_name].get("branch", "main")
             base = repo_name
             if isinstance(roots[repo_name], dict):
                 dest = roots[repo_name]["destination"]
