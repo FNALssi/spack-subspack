7633bbcd89c5f84b3203d85f380aa0c27a5da3ee
Marc Mengel <mengel@fnal.gov>
2026-07-30 15:28:39 -0500

    backwards

--- subspack/subspack.py
+++ subspack/subspack.py
@@ -325,11 +325,11 @@ def clone_various_configs(prefix, args):
         os.mkdir(basedir)
 
     if args.local_bootstrap:
+        root = f"{prefix}/var/spack/.bootstrap"
+    else:
         root = spack.config.get("bootstrap:root", default=None)
         if root:
             root = spack.util.path.canonicalize_path(root)
-    else:
-        root = f"{prefix}/var/spack/.bootstrap"
 
     with tmp_env("SPACK_ROOT", prefix):
         os.system(f"{prefix}/bin/spack bootstrap root {root} > /dev/null")
